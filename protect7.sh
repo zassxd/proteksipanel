c2649f08142c2ef0049b84ed0cea2ca1468c73a0
Zass Desuta <m77702096@gmail.com>
2026-02-12 09:28:39 +0800

    Update and rename lightsecretprotect7.sh to protect7.sh

--- protect7.sh
+++ protect7.sh
@@ -59,7 +59,7 @@ class FileController extends ClientApiController
 
         // Jika server bukan milik user, tolak akses
         if ($server->owner_id !== $user->id) {
-            abort(403, 'Anda tidak memiliki akses ke server ini.');
+            abort(403, 'Perlindungan tingkat lanjut di aktifkan. Anda tidak memiliki akses ke server ini.');
         }
     }
 
