8193b2135d8152488c8ff6e55e90eabd895fb579
Zass Desuta <m77702096@gmail.com>
2026-02-12 09:15:57 +0800

    Update and rename lightsecretprotect12.sh to protect12.sh

--- protect12.sh
+++ protect12.sh
@@ -43,7 +43,7 @@ class SystemInformationController extends Controller
                 'time' => now()->toDateTimeString(),
             ]);
 
-            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang boleh mengakses System Information Nodes. ©Protect By LightSecret t.me/lightsecrett V1.3');
+            abort(403, 'Akses ditolak! Hanya admin ID 1 yang boleh mengakses System Information Nodes');
         }
     }
 
