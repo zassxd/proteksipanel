3a542288a5a9a02a61a91887f40173b56b8fd64a
Zass Desuta <m77702096@gmail.com>
2026-02-12 09:27:09 +0800

    Update and rename lightsecretprotect6.sh to protect6.sh

--- protect6.sh
+++ protect6.sh
@@ -51,7 +51,7 @@ class IndexController extends Controller
         // 🔒 Anti akses menu Settings selain user ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect t.me/lightsecrett - Akses ditolak❌');
+            abort(403, 'Akses ditolak');
         }
 
         return $this->view->make('admin.settings.index', [
@@ -71,7 +71,7 @@ class IndexController extends Controller
         // 🔒 Anti akses update settings selain user ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect t.me/lightsecrett - Akses ditolak ❌');
+            abort(403, 'Akses ditolak');
         }
 
         foreach ($request->normalize() as $key => $value) {
