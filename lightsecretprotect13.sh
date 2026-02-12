9f35d795bef5bc3a341d42653bc98d1547105bb4
Zass Desuta <m77702096@gmail.com>
2026-02-12 09:16:59 +0800

    Update lightsecretprotect13.sh

--- lightsecretprotect13.sh
+++ lightsecretprotect13.sh
@@ -40,7 +40,7 @@ class NodeController extends Controller
         // === 🔒 FITUR TAMBAHAN: Anti akses selain admin ID 1 ===
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nodes. ©𝗣𝗿𝗼𝘁𝗲𝗰𝘁 𝗕𝘆 𝗟𝗶𝗴𝗵𝘁𝗦𝗲𝗰𝗿𝗲𝘁 t.me/lightsecrett 𝗩𝟭.𝟯');
+            abort(403, 'Akses ditolak! Hanya admin root admin yang dapat mengakses manajemen Node');
         }
         // ======================================================
 
